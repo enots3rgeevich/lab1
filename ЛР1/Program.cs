7df70c05cdc413948bfaf035c5c2f4d3d5824151
Enot Sergeevich <polya.marakulina@yandex.ru>
2019-11-11 10:03:15 +0300

    1 commit

---
+++ ЛР1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello     {0}!", name);
+
+            string first, last;
+            Console.WriteLine("Enter your first name: ");
+            first = Console.ReadLine();
+            Console.WriteLine("Enter your last name: ");
+            last = Console.ReadLine();
+            Console.WriteLine("Hello    {0} {1}!", first, last);
+            Console.ReadLine();
+        }
+    }
+}
